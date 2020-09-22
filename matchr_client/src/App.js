ea369cce0b3429fb6effcb689669830df5011e02
JamesLeggero <james.m.leggero@gmail.com>
2020-09-21 21:36:29 -0400

    comm 3 - wrote getDaters

--- matchr_client/src/App.js
+++ matchr_client/src/App.js
@@ -3,6 +3,17 @@ import Daters from './components/Hello'
 
 
 function App() {
+
+  const getDaters = async () => {
+    try {
+      const response = await fetch('http://localhost:3000/users')
+      const daters = response.json()
+      console.log(daters)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
   return (
     <div className="App">
       <Daters />
