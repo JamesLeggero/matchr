6c752246c46b894a5eedd8cd2410cb1cb13d3275
JamesLeggero <james.m.leggero@gmail.com>
2020-09-21 21:33:24 -0400

    comm 2 - added react app

---
+++ matchr_client/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Daters from './components/Hello'
+
+
+function App() {
+  return (
+    <div className="App">
+      <Daters />
+    </div>
+  );
+}
+
+export default App;
